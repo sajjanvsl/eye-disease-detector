6b29fac2d71576d07cc81c12bc0ef1c21ea53717
S P Sajjan <60458865+sajjanvsl@users.noreply.github.com>
2025-05-01 10:27:08 +0530

    Add files via upload

---
+++ cnn_model.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  1 08:22:26 2025
+
+@author: Admin
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class EyeCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(EyeCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+
+        self.fc1 = nn.Linear(128 * 28 * 28, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # 112x112
+        x = self.pool(F.relu(self.conv2(x)))  # 56x56
+        x = self.pool(F.relu(self.conv3(x)))  # 28x28
+        x = x.view(-1, 128 * 28 * 28)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
